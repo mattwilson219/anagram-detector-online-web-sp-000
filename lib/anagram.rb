65261a5be6b5e624ecfeb8bd0fc2995dd0b2d269
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-02 21:36:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,19 @@
-# Your code goes here!
+require 'pry'
+
 class Anagram
 
 attr_reader :name
 
 def intialize(name)
-@name = name
+  @name = name
 end
 
+def match(array)
+  array.keep_if do |str|
+    name.split('').sort == str.split('').sort
+  end
+end
+  
+end
 
 end
